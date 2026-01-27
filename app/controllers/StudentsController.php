815161abc59b3382c8ff6cc49611b1c8d73c8adb
PWarwq <vilux456@gmail.com>
2026-01-27 13:14:17 +0700

    wip: implement student controller

---
+++ app/controllers/StudentsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+class StudentsControllers
+{
+
+    public function index(): void
+    {
+
+        echo '<h1>Daftar Siswa</h1>';
+        echo '<p>Menampilkan daftar siswa</p>';
+
+    }
+    
+    public function create(): void
+    {
+
+        echo '<h1>Tambah Siswa</h1>';
+        echo '<p>Menampilkan form tambah siswa</p>';
+
+    }
+
+}
+
+
+?>
